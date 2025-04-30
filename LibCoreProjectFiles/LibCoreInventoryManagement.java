f6553be07a592cf76f0fa5fa10099cdea6ddb5ce
kartiktripathi <kartik.tripat@gmail.com>
2025-04-30 15:22:11 -0700

    Add LIBcore Java source files and data folder

--- LibCoreProjectFiles/LibCoreInventoryManagement.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-List<Resource> resourceList = new ArrayList<>();
-
-void addResource(Resource resource) {};
-void editResouce(Resource resource) {};
-void removeResouce(Resource resource) {};
